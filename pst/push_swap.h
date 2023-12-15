cda9803f2e39e17387381f36b677138ff4d87b83
Nadekko <139667516+Nadekko@users.noreply.github.com>
2023-12-15 20:00:37 +0100

    Add files via upload

--- pst/push_swap.h
+++ pst/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: andjenna <andjenna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 16:45:00 by andjenna          #+#    #+#             */
-/*   Updated: 2023/12/12 19:38:15 by andjenna         ###   ########.fr       */
+/*   Updated: 2023/12/15 16:24:16 by andjenna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,24 @@
 typedef struct s_node
 {
 	int				value;
-	int				current;
+	int				index;
 
 	struct s_node	*next;
 	struct s_node	*prev;
 }					t_node;
 
+typedef struct s_dlist
+{
+	size_t	size;
+	t_node	*first;
+	t_node	*last;
+}				t_dlist;
+
 /*					ERROR					*/
 void				ft_perror(char *err);
 long				ft_atol(char *nptr);
-void				ft_free_stack(t_node **stack);
-void				ft_exit_error(t_node **stack);
+void				ft_free_node(t_node **node);
+void				ft_exit_error(t_node **node);
 
 /*				HANDLE INPUT				*/
 char				**ft_split(char const *s, char c);
@@ -44,14 +51,15 @@ int					ft_checkinput(char **av, int ac);
 t_node				*ft_parse_arg(char **av);
 
 /*				INITIALISATION				*/
-t_node				*ft_init_stack(int value);
-void				ft_free_stack(t_node **stack);
-void				ft_exit_error(t_node **stack);
-void				ft_add_to_stack(t_node **stack, long value);
+/*
+t_node				*ft_init_node(int value);
+void				ft_free_node(t_node **node);
+void				ft_exit_error(t_node **node);
+void				ft_add_to_node(t_node **node, long value);
 t_node				*ft_lstlast(t_node *lst);
-void				ft_printf_stack(t_node *stack);
+void				ft_printf_node(t_node *node);
 void				ft_lstadd_back(t_node **lst, t_node *new);
-
+*/
 /*					COMMAND					*/
 
 #endif
